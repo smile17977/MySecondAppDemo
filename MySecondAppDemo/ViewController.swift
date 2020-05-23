9168d4adc7d2bedd850eacc6afe71087045c7de3
smile17977 <65645958+smile17977@users.noreply.github.com>
2020-05-23 22:50:40 +0300

    Initial Commit

---
+++ MySecondAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySecondAppDemo
+//
+//  Created by Kir Pir on 23.05.2020.
+//  Copyright © 2020 Kirill_Presnyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
